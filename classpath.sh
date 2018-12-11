2fde88f338eaf2a229f5273fedc36d4f1d67666f
Maximilian Mensing <max@maximilian-mensing.de>
2018-12-11 15:03:08 +0100

    cleaned up classpath.sh

--- classpath.sh
+++ classpath.sh
@@ -1,14 +1,3 @@
-
-CLASSPATH=.
-prepend_path()
-{
-  if ! eval test -z "\"\${$1##*:$2:*}\"" -o -z "\"\${$1%%*:$2}\"" -o -z "\"\${$1##$2:*}\"" -o -z "\"\${$1##$2}\"" ; then
-    eval "$1=$2:\$$1"
-  fi
-}
-
-prepend_path CLASSPATH `pwd`/antlr.jar
-prepend_path CLASSPATH `pwd`/bin.build/JSHOP2.jar
-
-echo $CLASSPATH
-export CLASSPATH
+echo "`pwd`/bin.build/JSHOP2.jar:`pwd`/antlr.jar:."
+export CLASSPATH="`pwd`/bin.build/JSHOP2.jar:`pwd`/antlr.jar:."
+echo 'has been set'
